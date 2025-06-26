e342fe3577e69a36eb3e0fa1b28f20e547374eaa
Jaymisom123 <jaymerego2015@gmail.com>
2025-06-26 18:00:39 -0300

    devbilsl initiall

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+import app from './app';
+
+dotenv.config();
+
+const PORT = Number(process.env.PORT) 
+
+const startServer = async () => {
+	try {
+		await app.listen({ port: PORT }).then(() => {
+			console.log(`Server is running on port ${PORT}`);
+		});
+	} catch (err) {
+		app.log.error(err);
+		process.exit(1);
+	}
+};
+
+startServer();
